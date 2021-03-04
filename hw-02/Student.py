5f43952a34d104dacec6b3131cf139bfab6ea6f3
jmounika16 <jettimou@msu.edu>
2021-03-04 02:27:43 -0500

    updated Student.py

--- hw-02/Student.py
+++ hw-02/Student.py
@@ -8,12 +8,29 @@ class Student():
     gpa : this is the student's curret gpa
     '''
     
-    def __init__(self, name='', gpa=0.0):
+    def __init__(self, name='', gpa=0.0, years=0):
         self.name = name
         self.gpa = gpa
+        self.years = year
         
     def get_name(self):
         '''
         This function prints the name of the student
         '''
         print("My name is", self.name)
+        
+    def enroll(self, courses):
+        listOfCourses = []
+        for course in courses:
+            listOfCourses.append(course)
+        return listOfCourses
+        
+    def display_courses(self, courses):
+        return "I am enrolled in:" + enroll(courses)
+    
+    def years_until_graduation(self):
+        return 4 - self.years
+        
+    
+            
+        
